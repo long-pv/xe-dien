2b5a53bb8d6cd08054796dbf4b2edc205a1c363e
longpv <long05062000@gmail.com>
2025-07-17 21:21:06 +0700

    no message

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -43,8 +43,57 @@ add_action('wp_head', 'xe_dien_pingback_header');
 // longpv
 // 
 // 
+// stop upgrading wp cerber plugin
+add_filter('site_transient_update_plugins', 'disable_plugins_update');
+function disable_plugins_update($value)
+{
+	// disable acf pro
+	if (isset($value->response['advanced-custom-fields-pro/acf.php'])) {
+		unset($value->response['advanced-custom-fields-pro/acf.php']);
+	}
 
+	// disable All-in-One WP Migration
+	if (isset($value->response['all-in-one-wp-migration-master/all-in-one-wp-migration.php'])) {
+		unset($value->response['all-in-one-wp-migration-master/all-in-one-wp-migration.php']);
+	}
+	return $value;
+}
 
+// Setup theme setting page
+if (function_exists('acf_add_options_page')) {
+	acf_add_options_page(
+		array(
+			'page_title' => 'Theme Settings',
+			'menu_title' => 'Theme Settings',
+			'menu_slug' => 'theme-settings',
+			'capability' => 'edit_posts',
+			'redirect' => false,
+			'position' => 80
+		)
+	);
+}
+
+// tối đa revision
+add_filter('wp_revisions_to_keep', function ($num, $post) {
+	return 3;
+}, 10, 2);
+
+// The function "write_log" is used to write debug logs to a file in PHP.
+function write_log($log = null, $title = 'Debug')
+{
+	if ($log) {
+		if (is_array($log) || is_object($log)) {
+			$log = print_r($log, true);
+		}
+
+		$timestamp = date('Y-m-d H:i:s');
+		$text = '[' . $timestamp . '] : ' . $title . ' - Log: ' . $log . "\n";
+		$log_file = WP_CONTENT_DIR . '/debug.log';
+		$file_handle = fopen($log_file, 'a');
+		fwrite($file_handle, $text);
+		fclose($file_handle);
+	}
+}
 
 
 
