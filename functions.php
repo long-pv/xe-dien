49b665cc0265b9951d2ef6b8c961afe1e6489178
longpv <long05062000@gmail.com>
2025-07-18 23:52:19 +0700

    no message

--- functions.php
+++ functions.php
@@ -187,7 +187,7 @@ function validate_title_post_admin()
 // thêm thương hiệu
 function xemer_theme_custom_admin_footer()
 {
-    echo 'Thanks for using WordPress. Powered by <a target="_blank" href="https://tramkienthuc.net/">Xemer Theme</a>.';
+    echo 'Thanks for using WordPress. Powered by <a target="_blank" href="https://www.facebook.com/longxemer">Long Pham</a>.';
 }
 add_filter('admin_footer_text', 'xemer_theme_custom_admin_footer');
 
