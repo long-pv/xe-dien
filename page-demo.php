8a0303f266cb993423367ba9e0d69ad171f21756
longpv <long05062000@gmail.com>
2025-07-24 23:42:48 +0700

    coding

---
+++ page-demo.php
@@ -0,0 +1,13 @@
+<?php
+// Template name: Demo
+get_header();
+?>
+<div class="py-section">
+    <div class="container">
+        <div class="editor">
+            <?php the_content(); ?>
+        </div>
+    </div>
+</div>
+<?php
+get_footer();
