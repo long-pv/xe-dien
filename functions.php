ffe06b84177e844f88101e3b369cdcec415c0997
longpv <long.phamvan@adamosoft.com>
2025-08-13 10:53:11 +0700

    tối ưu lại function

--- functions.php
+++ functions.php
@@ -13,52 +13,14 @@ if (! defined('_S_VERSION')) {
     define('_S_VERSION', '1.0.0');
 }
 
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
 function xe_dien_setup()
 {
-    /*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on xe dien, use a find and replace
-		* to change 'xe-dien' to the name of your theme in all the template files.
-		*/
+    $GLOBALS['content_width'] = apply_filters('xe_dien_content_width', 640);
     load_theme_textdomain('xe-dien', get_template_directory() . '/languages');
-
-    // Add default posts and comments RSS feed links to head.
     add_theme_support('automatic-feed-links');
-
-    /*
-		* Let WordPress manage the document title.
-		* By adding theme support, we declare that this theme does not use a
-		* hard-coded <title> tag in the document head, and expect WordPress to
-		* provide it for us.
-		*/
     add_theme_support('title-tag');
-
-    /*
-		* Enable support for Post Thumbnails on posts and pages.
-		*
-		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		*/
     add_theme_support('post-thumbnails');
-
-    // This theme uses wp_nav_menu() in one location.
-    register_nav_menus(
-        array(
-            'menu-1' => esc_html__('Primary', 'xe-dien'),
-        )
-    );
-
-    /*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
+    add_theme_support('customize-selective-refresh-widgets');
     add_theme_support(
         'html5',
         array(
@@ -71,27 +33,6 @@ function xe_dien_setup()
             'script',
         )
     );
-
-    // Set up the WordPress core custom background feature.
-    add_theme_support(
-        'custom-background',
-        apply_filters(
-            'xe_dien_custom_background_args',
-            array(
-                'default-color' => 'ffffff',
-                'default-image' => '',
-            )
-        )
-    );
-
-    // Add theme support for selective refresh for widgets.
-    add_theme_support('customize-selective-refresh-widgets');
-
-    /**
-     * Add support for core custom logo.
-     *
-     * @link https://codex.wordpress.org/Theme_Logo
-     */
     add_theme_support(
         'custom-logo',
         array(
@@ -101,42 +42,15 @@ function xe_dien_setup()
             'flex-height' => true,
         )
     );
-}
-add_action('after_setup_theme', 'xe_dien_setup');
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function xe_dien_content_width()
-{
-    $GLOBALS['content_width'] = apply_filters('xe_dien_content_width', 640);
-}
-add_action('after_setup_theme', 'xe_dien_content_width', 0);
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function xe_dien_widgets_init()
-{
-    register_sidebar(
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus(
         array(
-            'name'          => esc_html__('Sidebar', 'xe-dien'),
-            'id'            => 'sidebar-1',
-            'description'   => esc_html__('Add widgets here.', 'xe-dien'),
-            'before_widget' => '<section id="%1$s" class="widget %2$s">',
-            'after_widget'  => '</section>',
-            'before_title'  => '<h2 class="widget-title">',
-            'after_title'   => '</h2>',
+            'menu-1' => esc_html__('Primary', 'xe-dien'),
         )
     );
 }
-add_action('widgets_init', 'xe_dien_widgets_init');
+add_action('after_setup_theme', 'xe_dien_setup', 0);
 
 /**
  * Enqueue scripts and styles.
@@ -211,7 +125,6 @@ function write_log($log = null, $title = 'Debug')
     }
 }
 
-// Tạo menu theme settings chung
 // Setup theme setting page
 if (function_exists('acf_add_options_page')) {
     // Trang cài đặt chính
@@ -224,7 +137,6 @@ if (function_exists('acf_add_options_page')) {
         'position'   => 80
     ));
 }
-// end
 
 // stop upgrading ACF pro plugin
 add_filter('site_transient_update_plugins', 'disable_plugins_update');
@@ -237,9 +149,9 @@ function disable_plugins_update($value)
     return $value;
 }
 
-// tối đa revision
+// tối đa 5 revision cho mỗi bài viết
 add_filter('wp_revisions_to_keep', function ($num, $post) {
-    return 3;
+    return 5;
 }, 10, 2);
 
 // tắt thông báo rác
@@ -247,3 +159,39 @@ add_action('admin_head', function () {
     remove_all_actions('admin_notices');
     remove_all_actions('all_admin_notices');
 });
+
+// tự động cài đặt lựa chọn hình ảnh căn giữa
+function set_default_image_settings_on_login($user_login, $user)
+{
+    global $wpdb;
+
+    $user_id = $user->ID;
+    $prefix = $wpdb->prefix;
+    $meta_key = $prefix . 'user-settings';
+    $current_settings = get_user_meta($user_id, $meta_key, true);
+
+    if (strpos($current_settings, '&align=') !== false) {
+        $current_settings = preg_replace('/&align=([^"]*)/', '&align=center', $current_settings);
+    } else {
+        $current_settings .= '&align=center';
+    }
+
+    if (strpos($current_settings, '&imgsize=') !== false) {
+        $current_settings = preg_replace('/&imgsize=([^"]*)/', '&imgsize=center', $current_settings);
+    } else {
+        $current_settings .= '&imgsize=center';
+    }
+
+    update_user_meta($user_id, $meta_key, $current_settings);
+}
+add_action('wp_login', 'set_default_image_settings_on_login', 10, 2);
+
+// thêm item dropdown sửa dụng cho mobile
+function add_dropdown_arrow_to_menu($items, $args)
+{
+    if (!empty($args->theme_location)) {
+        $items = preg_replace('/(<a.*?>)(.*)<\/a>/i', '<span class="dropdown-arrow"></span>$1$2</a>', $items);
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'add_dropdown_arrow_to_menu', 10, 2);
