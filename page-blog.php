cfb5316af27b307d6b847a9356fd12a7d3f2c359
Youngboy55 <phamvanvu411.nd@gmail.com>
2025-08-20 21:56:00 +0700

    task 1.  "Blog page + Detail page"

---
+++ page-blog.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package xe_dien
+ */
+
+get_header();
+?>
+
+<section id="vinfast-2025" class="blog__section">
+	<div class="container">
+		<!-- Heading -->
+		<h2 class="blog__heading">
+			Tin Tức Mới Nhất
+		</h2>
+
+		<!-- Content Post -->
+		<div class="row">
+			<div class="col-lg-4">
+				<article class="post__item" data-mh="post_item">
+					<!-- Thumnail Post -->
+					<a class="d-block post__thumbnail" href="#">
+						<!-- Image -->
+						<img fetchpriority="high" width="650" height="432"
+							src="https://minhdigi.com/wp-content/uploads/2025/02/broken-link-la-gi.jpg"
+							class="attachment-large size-large wp-post-image" alt="broken-link-la-gi" decoding="async"
+							srcset="https://minhdigi.com/wp-content/uploads/2025/02/broken-link-la-gi.jpg 650w, https://minhdigi.com/wp-content/uploads/2025/02/broken-link-la-gi-300x199.jpg 300w"
+							sizes="(max-width: 650px) 100vw, 650px">
+					</a>
+
+					<!-- Content Post -->
+					<div class="post__content" data-mh="post__content">
+						<!-- Title Post -->
+						<a class="d-flex post__title" href="#">
+							<h3 class="line-2">
+								Vingroup phát động chiến dịch “Mãnh liệt Tinh thần Việt Nam – Vì Tương lai Xanh”
+							</h3>
+						</a>
+
+						<!-- Excerpt Post -->
+						<p class="post__desc line-3">
+							Lorem ipsum dolor sit amet consectetur. Nisl erat massa eget neque at. Aliquam et mauris at
+							semper. Accumsan
+							massa netus fringilla nibh dictum orci sit ultricies ac. Adipiscing nisl sapien pellentesque
+							quis elementum
+							lacus proin in habitant. Purus nibh vel nunc nibh vel venenatis posuere. Dignissim amet
+							lobortis tincidunt
+							in. Nisl eu neque senectus mattis elementum nunc in. Quis nulla etiam blandit semper
+							facilisis. Vitae
+							pretium lectus morbi dolor platea augue.
+						</p>
+
+						<!-- Info Post -->
+						<div class="post__info d-flex">
+
+							<!-- Author Post -->
+							<div class="post__category">
+								<!-- Icon -->
+								<svg></svg>
+								<!-- Link Author -->
+								<a href="#" class="post__category--name">
+									VinFast Đức Nghĩa
+								</a>
+							</div>
+							<div class="post__date">
+								Thứ ba, 19/8/2025, 08:00
+							</div>
+
+						</div>
+					</div>
+				</article>
+			</div>
+			<div class="col-lg-4">
+				<article class="post__item" data-mh="post_item">
+					<!-- Thumnail Post -->
+					<a class="d-block post__thumbnail" href="#">
+						<!-- Image -->
+						<img fetchpriority="high" width="650" height="432"
+							src="https://minhdigi.com/wp-content/uploads/2025/02/broken-link-la-gi.jpg"
+							class="attachment-large size-large wp-post-image" alt="broken-link-la-gi" decoding="async"
+							srcset="https://minhdigi.com/wp-content/uploads/2025/02/broken-link-la-gi.jpg 650w, https://minhdigi.com/wp-content/uploads/2025/02/broken-link-la-gi-300x199.jpg 300w"
+							sizes="(max-width: 650px) 100vw, 650px">
+					</a>
+
+					<!-- Content Post -->
+					<div class="post__content" data-mh="post__content">
+						<!-- Title Post -->
+						<a class="d-flex post__title" href="#">
+							<h3 class="line-2">
+								Vingroup phát động chiến dịch “Mãnh liệt Tinh thần Việt Nam – Vì Tương lai Xanh”
+							</h3>
+						</a>
+
+						<!-- Excerpt Post -->
+						<p class="post__desc line-3">
+							Lorem ipsum dolor sit amet consectetur. Nisl erat massa eget neque at. Aliquam et mauris at
+							semper. Accumsan
+							massa netus fringilla nibh dictum orci sit ultricies ac. Adipiscing nisl sapien pellentesque
+							quis elementum
+							lacus proin in habitant. Purus nibh vel nunc nibh vel venenatis posuere. Dignissim amet
+							lobortis tincidunt
+							in. Nisl eu neque senectus mattis elementum nunc in. Quis nulla etiam blandit semper
+							facilisis. Vitae
+							pretium lectus morbi dolor platea augue.
+						</p>
+
+						<!-- Info Post -->
+						<div class="post__info d-flex">
+
+							<!-- Author Post -->
+							<div class="post__category">
+								<!-- Icon -->
+								<svg>
+								</svg>
+								<!-- Link Author -->
+								<a href="#" class="post__category--name">
+									VinFast Đức Nghĩa
+								</a>
+							</div>
+							<div class="post__date">
+								Thứ ba, 19/8/2025, 08:00
+							</div>
+
+						</div>
+					</div>
+				</article>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Post Item End -->
+
+<?php
+get_footer();
