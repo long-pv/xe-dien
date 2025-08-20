5090a2fd66c036a2e1de577ca22fcd4e63e5ea68
longpv <long05062000@gmail.com>
2025-08-20 19:49:53 +0700

    no message

---
+++ page-about_us.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Template name: Trang giới thiệu
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package xe_dien
+ */
+
+get_header();
+?>
+
+<div class="about_us">
+	
+</div>
+
+<?php
+get_footer();
